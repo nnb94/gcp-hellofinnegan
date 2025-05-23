5f7fc78840c6112c218dfba2da3cca2c9ea96d4e
nnb94 <nirvannb94@gmail.com>
2025-05-23 14:21:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     """Example Hello World route."""
-    name = os.environ.get("NAME", "Finnegan broski")
+    name = os.environ.get("NAME", "Finnegan brosk")
     return f"Hello {name}!"
 
 
